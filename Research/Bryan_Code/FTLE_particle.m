7ddd43e6eba5ab5f126cf0e862c0cf1a5f2fa7a0
Bryan <boakley@wisc.edu>
2022-11-15 11:24:43 -0700

    Computing 4 points FTLE and Strain increment. To Do: 4 point Strain

--- Research/Bryan_Code/FTLE_particle.m
+++ Research/Bryan_Code/FTLE_particle.m
@@ -10,7 +10,8 @@ kk = 256;
 L = 3;
 %Full period or time unit
 Period=1;
-%Find a point of high, med, low stretching. Here are the coords:
+%Find a point of high, med, low stretching. Here are the coords, chosen
+%after 99 time steps:
 rowIndexHigh = 151; %y-coord index of High
 colIndexHigh = 95; %x-coord index of High
 rowIndexMed = 127;
@@ -84,6 +85,16 @@ for ti=1:N_Basetime
     FTLEHistoryMed = zeros(1,no_of_steps);
     FTLEHistoryLow = zeros(1,no_of_steps);
 
+    %Keep track of the FTLE for high, med, low. Using the 4 points
+    %algorithm
+    FTLEHistoryHighPoints = zeros(1,no_of_steps);
+    FTLEHistoryMedPoints = zeros(1,no_of_steps);
+    FTLEHistoryLowPoints = zeros(1,no_of_steps);
+
+    %Keep track of the Strain for high, med, low
+    StrainHistoryHigh = zeros(1,no_of_steps);
+    StrainHistoryMed = zeros(1,no_of_steps);
+    StrainHistoryLow = zeros(1,no_of_steps);
 
     for i = 1:no_of_steps
         i;
@@ -114,6 +125,10 @@ for ti=1:N_Basetime
         data=reshape(data,kk,kk,3);
         u1 = data(:,:,1)' ;
         v1 = data(:,:,2)' ;
+
+        u1Data=u1;
+        v1Data=v1;
+
         u1Extend = [u1, u1, u1; u1, u1, u1; u1, u1, u1];
         v1Extend = [v1, v1, v1; v1, v1, v1; v1, v1, v1];
         u1 = u1Extend;
@@ -131,12 +146,13 @@ for ti=1:N_Basetime
         data=reshape(data,kk,kk,3);
         u2 = data(:,:,1)' ;
         v2 = data(:,:,2)' ;
+
+
+
         u2Extend = [u2, u2, u2; u2, u2, u2; u2, u2, u2];
         v2Extend = [v2, v2, v2; v2, v2, v2; v2, v2, v2];
         u2 = u2Extend;
         v2 = v2Extend;
-
-        
 
         loaded = [-1000 -1000];
         index1=-10000;
@@ -238,15 +254,13 @@ for ti=1:N_Basetime
   
         
 %         if i == 20
-       if false
-            figure;
-            quiver(u1,u2)
-            title('flow')
-
-            figure;
-            quiver(dx,dy)
-            title('displacement')
-        end
+%             figure;
+%             quiver(u1,u2)
+%             title('flow')
+% 
+%             figure;
+%             quiver(dx,dy)
+%             title('displacement')
 
        %Then update xnow:
         xnow = xnow + dx;
@@ -359,6 +373,199 @@ for ti=1:N_Basetime
        FTLEHistoryMed(i) = interp2(xa,ya,dle,xCoordHistoryMed(i),yCoordHistoryMed(i),inter,0);
        FTLEHistoryLow(i) =  interp2(xa,ya,dle,xCoordHistoryLow(i),yCoordHistoryLow(i),inter,0);
 
+       
+        
+        %*******************************************************************
+        %*******************************************************************
+
+
+        %This time, compute the FTLE at every time step a different way.
+        %Consider 4 points close to my trajectory.
+        h = 1/(256*20);
+        %Select High, Med, Low
+        for caseSelect = 1:3
+            if caseSelect == 1
+                pNow = [xCoordHistoryHigh(i), yCoordHistoryHigh(i)];
+            elseif caseSelect == 2
+                pNow = [xCoordHistoryMed(i), yCoordHistoryMed(i)];
+            else
+                pNow = [xCoordHistoryLow(i), yCoordHistoryLow(i)];
+            end
+    
+            pLeft = pNow+[-h,0];
+            pRight = pNow+[h,0];
+            pUp = pNow+[0,h];
+            pDown = pNow+[0,-h];
+       
+            pointsX = [pLeft(1), pNow(1), pRight(1)];
+            pointsY = [pDown(2), pNow(2), pUp(2)];
+    
+            [pointsXX, pointsYY]=meshgrid(pointsX,pointsY); 
+            
+    
+            %Now evolve the 3 by 3 grid of points 1 time step:
+    
+    
+            uinterp = u1 + (t0 - (index1-1)*ft)*(u2-u1)/ft;
+            vinterp = v1 + (t0 - (index1-1)*ft)*(v2-v1)/ft;
+    
+            
+           
+    
+            xm=mod(pointsXX,L);ym=mod(pointsYY,L);
+            urhs1 = interp2(xx,yy,uinterp,xm,ym,inter,0);
+            vrhs1 = interp2(xx,yy,vinterp,xm,ym,inter,0);          
+                %Instead of interping on a subset xm (a 256 grid of len L/3) of xx (a 768 grid of len L), 
+                %we will interp on a finer set xm of xxcoarse (a 256 grid of len L):
+                    %urhs1 = interp2(xxcoarse,yycoarse,uinterp,xm,ym,inter,0);
+                    %vrhs1 = interp2(xxcoarse,yycoarse,vinterp,xm,ym,inter,0);
+    
+    
+    %*******************************************************************
+            x1 = pointsXX + urhs1*deltat/2;
+            y1 = pointsYY + vrhs1*deltat/2;
+            %min(min(x1)) %Yes, x1 does leave the set [1,2]^2 a bit. So, we should mod it back.
+            %max(max(x1))
+            t1 = t0 + deltat/2;
+            index1n = floor(t1/ft) + 1;
+            if index1n~=index1
+                index1 = index1n;
+                index2 = index1 + 1;
+                %checkload;
+            end 
+            uinterp = u1 + (t1 - (index1-1)*ft)*(u2-u1)/ft;
+            vinterp = v1 + (t1 - (index1-1)*ft)*(v2-v1)/ft;
+    
+            %xm=mod(pointsXX,L);ym=mod(pointsYY,L);
+            xm=mod(x1,L);ym=mod(y1,L);   %***********xm should update to mod(x1,L)   
+            
+            urhs2 = interp2(xx,yy,uinterp,xm,ym,inter,0);
+            vrhs2 = interp2(xx,yy,vinterp,xm,ym,inter,0);
+                %Instead of interping on a subset xm (a 256 grid of len L/3) of xx (a 768 grid of len L), 
+                %we will interp on a finer set xm of xxcoarse (a 256 grid of len L):
+                    %urhs2 = interp2(xxcoarse,yycoarse,uinterp,xm,ym,inter,0);
+                    %vrhs2 = interp2(xxcoarse,yycoarse,vinterp,xm,ym,inter,0);
+     %*************************************************************
+            x2 = pointsXX + urhs2*deltat/2;
+            y2 = pointsYY + vrhs2*deltat/2;
+            t2 = t0 + deltat/2;   
+    
+            %xm=mod(pointsXX,L);ym=mod(pointsYY,L);
+            xm=mod(x2,L);ym=mod(y2,L); 
+    
+            urhs3 = interp2(xx,yy,uinterp,xm,ym,inter,0);
+            vrhs3 = interp2(xx,yy,vinterp,xm,ym,inter,0);   
+                %Instead of interping on a subset xm (a 256 grid of len L/3) of xx (a 768 grid of len L), 
+                %we will interp on a finer set xm of xxcoarse (a 256 grid of len L):
+                    %urhs3 = interp2(xxcoarse,yycoarse,uinterp,xm,ym,inter,0);
+                    %vrhs3 = interp2(xxcoarse,yycoarse,vinterp,xm,ym,inter,0);
+     %****************************************************************
+            x3 = pointsXX + urhs3*deltat;
+            y3 = pointsYY + vrhs3*deltat;
+            t3 = t0 + deltat;
+            index1n = floor(t3/ft) + 1;
+            if index1n~=index1
+                index1 = index1n;
+                index2 = index1 + 1;
+                %checkload;
+            end 
+            
+            uinterp = u1 + (t3 - (index1-1)*ft)*(u2-u1)/ft;
+            vinterp = v1 + (t3 - (index1-1)*ft)*(v2-v1)/ft;
+            
+            %xm=mod(pointsXX,L);ym=mod(pointsYY,L);
+            xm=mod(x3,L);ym=mod(y3,L);  
+    
+            urhs4 = interp2(xx,yy,uinterp,xm,ym,inter,0);
+            vrhs4 = interp2(xx,yy,vinterp,xm,ym,inter,0); 
+                %Instead of interping on a subset xm (a 256 grid of len L/3) of xx (a 768 grid of len L), 
+                %we will interp on a finer set xm of xxcoarse (a 256 grid of len L):
+                    %urhs4 = interp2(xxcoarse,yycoarse,uinterp,xm,ym,inter,0);
+                    %vrhs4 = interp2(xxcoarse,yycoarse,vinterp,xm,ym,inter,0);
+     %********************************************************************   
+        
+            dx = (deltat/6)*(urhs1 + 2*urhs2 + 2*urhs3 + urhs4);
+            dy = (deltat/6)*(vrhs1 + 2*vrhs2 + 2*vrhs3 + vrhs4);
+    
+            pointsXXafter = pointsXX + dx;
+            pointsYYafter = pointsYY + dy;
+    
+    %         figure;
+    %         scatter(pointsXX,pointsYY,[],'filled')
+    %         hold on;
+    %         scatter(pointsXXafter,pointsYYafter)
+    %         hold off;
+    
+    
+    %         [G11, G12] = gradient(X,xss,yss);
+    %         [G21, G22] = gradient(Y,xss,yss);
+            G11 = (1/(2*h))*(pointsXXafter(2,3) - pointsXXafter(2,1));
+            G12 = (1/(2*h))*(pointsXXafter(3,2) - pointsXXafter(1,2));
+    
+            G21 = (1/(2*h))*(pointsYYafter(2,3) - pointsYYafter(2,1));
+            G22 = (1/(2*h))*(pointsYYafter(3,2) - pointsYYafter(1,2));
+            %compute Cauchy-Green tensor
+            CG11 = G11.*G11 + G21.*G21 ;
+            CG12 = G11.*G12 + G21.*G22 ;
+            
+            CG21 = G12.*G11 + G22.*G21 ;
+            CG22 = G12.*G12 + G22.*G22 ;
+            
+            Tr=CG11+CG22;
+            Del=CG11.*CG22-CG12.*CG21;
+            lam=Tr/2+sqrt(Tr.^2-4*Del)/2;
+            %dle=log(lam)/(2)
+            %dle=log(lam)/(2*INTTIME);            %Use this for net difference
+            dle=log(lam)/(2*deltat);   %Use this for incremental diff
+
+
+            %Now save the dle history
+            if caseSelect == 1
+                FTLEHistoryHighPoints(i) = dle;
+            elseif caseSelect == 2
+                FTLEHistoryMedPoints(i) = dle;
+            else
+                FTLEHistoryLowPoints(i) = dle;
+            end
+    
+            
+            %We want to compare this FTLE 4 particle to Strain 4 particle:
+
+            if caseSelect == 1
+                rowIndex = rowIndexHigh;
+                colIndex = colIndexHigh;
+            elseif caseSelect == 2
+                rowIndex = rowIndexMed;
+                colIndex = colIndexHigh;
+            else
+                rowIndex = rowIndexLow;
+                colIndex = colIndexHigh;
+            end
+
+            A11 = (256/2)*(u1Data(rowIndex+1,colIndex) - u1Data(rowIndex-1,colIndex));
+            A12 = (256/2)*(u1Data(rowIndex,colIndex+1) - u1Data(rowIndex,colIndex-1));
+    
+            A21 = (256/2)*(v1Data(rowIndex+1,colIndex) - v1Data(rowIndex-1,colIndex));
+            A22 = (256/2)*(v1Data(rowIndex,colIndex+1) - v1Data(rowIndex,colIndex-1));
+            
+            A = [A11, A12; A21, A22];
+
+            ASym = .5*(A + A');
+
+            lamA = eig(ASym);
+            LAM = lamA(2);
+            strain=log(LAM);  %Use this for incremental diff
+
+            if caseSelect == 1
+                StrainHistoryHigh(i) = strain;
+            elseif caseSelect == 2
+                StrainHistoryMed(i) = strain;
+            else
+                StrainHistoryLow(i) = strain;
+            end
+
+        end
+
 
     end
 
@@ -366,9 +573,9 @@ for ti=1:N_Basetime
 
     dle_2d;
     
-    FTLEHigh = dle(rowIndexHigh,colIndexHigh)
-    FTLEMed = dle(rowIndexMed,colIndexMed)
-    FTLELow = dle(rowIndexLow,colIndexLow)
+    FTLEHigh = dle(rowIndexHigh,colIndexHigh);
+    FTLEMed = dle(rowIndexMed,colIndexMed);
+    FTLELow = dle(rowIndexLow,colIndexLow);
 
 
 
@@ -386,22 +593,39 @@ for ti=1:N_Basetime
 
     figure;
     subplot(3,1,1)
-    plot(FTLEHistoryHigh)
+    plot(FTLEHistoryHigh,'k')
+    hold on;
+    plot(FTLEHistoryHighPoints,'b')
+    hold on;
+    plot(StrainHistoryHigh,'r')
     hold on;
     plot(FTLEHigh*ones(1,no_of_steps))
     hold off;
+    legend('FTLE increment','FTLE 4 Points','Strain increment','Net FTLE')
     title('FTLE High')
+
     subplot(3,1,2)
-    plot(FTLEHistoryMed) 
+    plot(FTLEHistoryMed,'k') 
+    hold on;
+    plot(FTLEHistoryMedPoints,'b')
+    hold on;
+    plot(StrainHistoryMed,'r')
     hold on;
     plot(FTLEMed*ones(1,no_of_steps))
     hold off;
+    legend('FTLE increment','FTLE 4 Points','Strain increment','Net FTLE')
     title('FTLE Med')
+
     subplot(3,1,3)
-    plot(FTLEHistoryLow)
+    plot(FTLEHistoryLow,'k')
+    hold on;
+    plot(FTLEHistoryLowPoints,'b')
+    hold on;
+    plot(StrainHistoryLow,'r')
     hold on;
     plot(FTLELow*ones(1,no_of_steps))
     hold off;
+    legend('FTLE increment','FTLE 4 Points','Strain increment','Net FTLE')
     title('FTLE Low')
 
     
