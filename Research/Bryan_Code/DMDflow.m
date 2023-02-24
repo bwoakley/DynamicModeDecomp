9b15e326d6446b2a22589a2c080a6ae093f1ba57
Bryan <boakley@wisc.edu>
2023-02-24 12:43:45 -0700

    Fix r, vary N.
    Create monoN.jpg

--- Research/Bryan_Code/DMDflow.m
+++ Research/Bryan_Code/DMDflow.m
@@ -22,7 +22,7 @@ if false
     
     for r = rVec
         
-        [error, lambda] = DMDpred(pred,N,r);
+        [error, lambda, S] = DMDpred(pred,N,r);
         plot(error)
         hold on;
         
@@ -45,8 +45,11 @@ end
 
 
 %Activate block to plot singular values and eigenvalues
-if true
-    
+%Here I used:
+%N = 10
+%r = 5, 6, or 9
+if false
+
     r = 9;   %How many modes to use
     figure;
         
@@ -70,6 +73,45 @@ end
 
 
 
+
+
+
+%Now fix r and vary N.
+if true
+    r = 5;   %How many modes to use
+    
+    figure;
+    nVec = [10,20,30,40,50]; %Max pred to take is 100-N
+    
+    for N = nVec
+    
+        pred = 100-N;
+    
+        [error, lambda, S] = DMDpred(pred,N,r);
+        plot(error)
+        hold on;
+        
+    end
+    
+    hold off;
+    legend('N=10','N=20','N=30','N=40','N=50')
+    title('Error for different N, given r = 5.')
+    xlabel('Number of time steps predicted')
+    ylabel('Error')
+end
+
+% 
+% figure;
+% plot(real(lambda),'*-')
+% hold on; 
+% plot(imag(lambda),'o-')
+% hold off;
+% title('Real and imaginary parts of the eigenvalues $\Lambda$ of $\widetilde{A}$','interpreter','latex')
+% legend('Real part','Imaginary part')
+% 
+% figure;
+% plot(diag(S))
+% title('Singular values of $X_1$','interpreter','latex')
 
 
 
