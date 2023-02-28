a8fabc54ad786064edba87b3f330119ee82b3d77
Bryan <boakley@wisc.edu>
2023-02-28 10:47:46 -0700

    Code showing SVD of X1 for linear flow. Confirm only one sing val
    Also W-->Z and U-->W for notational convenience.

--- Research/Bryan_Code/DMD.m
+++ Research/Bryan_Code/DMD.m
@@ -19,14 +19,15 @@ function [Phi ,omega ,lambda ,b,Xdmd,S,uMax] = DMD(X1,X2,r,dt)
 % b, a vector of magnitudes of modes Phi
 % Xdmd, the data matrix reconstructed by Phi , omega , b
 %% DMD
-[U, S, V] = svd(X1, 'econ');
-r = min(r, size(U,2));
-U_r = U(:, 1:r); % truncate to rank-r
+[W, S, V] = svd(X1, 'econ')
+r = min(r, size(W,2));
+W_r = W(:, 1:r); % truncate to rank-r
 S_r = S(1:r, 1:r);
 V_r = V(:, 1:r);
-Atilde = U_r.' * X2 * V_r / S_r; % low -rank dynamics
-[W_r , D] = eig(Atilde);
-Phi = X2 * V_r / S_r * W_r; % DMD modes
+Atilde = W_r.' * X2 * V_r / S_r; % low -rank dynamics
+
+[Z_r , D] = eig(Atilde);
+Phi = X2 * V_r / S_r * Z_r; % DMD modes
 lambda = diag(D); % discrete -time eigenvalues
 omega = log(lambda)/dt; % continuous-time eigenvalues
 %% Compute DMD mode amplitudes b
