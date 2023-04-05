3380f6a265e87bd30bdc62621c1a3c441f8a6daf
boakley <bwoakley@asu.edu>
2023-04-05 14:54:00 -0700

    Meeting update

--- Research/Bryan_Code/DMDflow.m
+++ Research/Bryan_Code/DMDflow.m
@@ -3,14 +3,13 @@ close all;
 clc;
 format long;
 
+plot_eval = true;    %Set true to plot top evals
+
 make_movie = false;         %Set to true to plot movie of evals
 if make_movie                   
     v = VideoWriter('temp.avi');
     open(v)
 end
-
-
-
 
 kk = 256;                    %Grid is kk by kk
 rows = (kk^2)*2;             %Number of rows in the snapshots
@@ -24,7 +23,7 @@ if N+pred>500
     disp('Need more data')
 end
 
-no_Windows = 399;            %How many windows of length N to compute
+no_Windows = 50;            %How many windows of length N to compute
 shift = 0;                %Shift starting window. We will look from N = shift to N = shift+no_Windows
 
 
@@ -33,14 +32,13 @@ flowCase = 1;   %flowCase decides what flow to use.
                 % flowCase = 2 means Linear flow (-x,y)
                 % flowCase = 3 means Linear flow with time dependent amplitute (1-t/10). 
 
-%[ Xpred, Xdmd, stateVecs, Phi, lambda, S, Atilde, X1 ] = DMDpred( pred, N, r, flowCase, kk, no_Windows);
 
 
 % Activate block to plot error for various r
-%Here I used:
-% pred = 90;                   
-% N = 10; 
-if false
+ if false
+    %Here I used:
+    % pred = 90;                   
+    % N = 10;
     flowCase = 1; %flowCase decides what flow to use. flowCase = 1 means 'turb'
     figure;
     rVec = [1,2,3,4,5,6,7,8,9]; %Max r to take is N-1
@@ -68,15 +66,12 @@ if false
 
 end
 
-
-% Activate block to:
-% Plot error and
-% plot singular values and eigenvalues
-%Consider :
-%N = 10
-%r = 5, 6, or 9
+% Activate block to Plot error, plot singular values and eigenvalues
 if false
-    
+    %Consider :
+    %N = 10
+    %r = 5, 6, or 9
+
     %Now analize the error at each time step
     error = zeros(1,pred);
     %errorC = zeros(1,pred);
@@ -142,7 +137,6 @@ if false
 
 end
 
-
 % Now fix r and vary N.
 if false
 
@@ -168,7 +162,6 @@ if false
     xlabel('Number of time steps predicted')
     ylabel('Error')
 end
-
 
 % Plot the predicted flow
 if false
@@ -196,7 +189,6 @@ if false
 
 
 end
-
 
 % Plot the singular values and DMD modes
 if false
@@ -254,13 +246,9 @@ end
     % s4 = zeros(1,no_Windows);
 
 
-%Keep track of coeff b over no_Windows
-%     bVecAbs = zeros(r, no_Windows);
-%     domModeVec = zeros(1, no_Windows);
-%     domEval = zeros(1, no_Windows);
 
-%Now update above to track top number of evals
-top = 20;    %Number of top eval to track
+%Now track top number of evals
+top = 6;    %Number of top eval to track
 topEval = zeros(top, no_Windows);
 
 
@@ -302,135 +290,106 @@ for start_index = 1 : no_Windows
     end
 
     if false   %Plot DMD modes
-       
-    %Atilde
-    %[Z_r , D] = eig(Atilde);
-
-    %Plot the e.vals.
-%     ss = lambda;
-%     s1(i) = real(ss(1));
-%     s2(i) = real(ss(2));
-%     s3(i) = real(ss(3));
-%     s4(i) = real(ss(4));
+        %Atilde
+        %[Z_r , D] = eig(Atilde);
     
-    %Plot the DMD modes
-%     figure;
-%     Ut=reshape(Phi(1:kk^2,1),kk,kk);
-%     Ut=Ut';
-%     if start_index == 1
-%         U1W1 = Ut;
-%     elseif start_index == 2
-%         U1W2 = Ut;
-%     end
+        %Plot the e.vals.
+        ss = lambda;
+        s1(i) = real(ss(1));
+        s2(i) = real(ss(2));
+        s3(i) = real(ss(3));
+        s4(i) = real(ss(4));
         
-%     pcolor(real(Ut));shading interp;daspect([1 1 1]);colorbar;hold on;
-%     xlabel('X');ylabel('Y')
-%     set(gca,'position',[0.64 .1 .3 .8])
-%     title('DMD mode 1','fontsize',18)
+        %Plot the DMD modes
+        figure;
+        Ut=reshape(Phi(1:kk^2,1),kk,kk);
+        Ut=Ut';
+        if start_index == 1
+            U1W1 = Ut;
+        elseif start_index == 2
+            U1W2 = Ut;
+        end
+            
+        pcolor(real(Ut));shading interp;daspect([1 1 1]);colorbar;hold on;
+        xlabel('X');ylabel('Y')
+        set(gca,'position',[0.64 .1 .3 .8])
+        title('DMD mode 1','fontsize',18)
     end
 
 
-    if true   %Find dominant mode
+    if plot_eval   %Find dominant mode
         bAbs = abs(b);
         [sort_bAbs, idx] = sort(bAbs,'descend');
 
         for modeNumber = 1:top
             topEval(modeNumber,i) = lambda(idx(modeNumber));
         end
-    
-     
-
-        
-%         bVecAbs(:,i) = bAbs;
-%         [domModeVal, domMode] = max(bAbs);
-%         domModeVec(i) =  domMode;
-    
-    %     How to find the conjugate of the dominant mode?
-    %     temp = bAbs;
-    %     temp(domMode) =  temp(domMode)- 1*1e-10;
-    %     temp
-
-%         domEval(i) = lambda(domMode);
-
-        
-    
     end
 
 end
 
 %Plot change in DMD mode 1
 if false
-    % pcolor(real(U1 - U2));shading interp;daspect([1 1 1]);colorbar;hold on;
-    % xlabel('X');ylabel('Y')
-    % title('Difference of U1 and U2','fontsize',18)
+    pcolor(real(U1 - U2));shading interp;daspect([1 1 1]);colorbar;hold on;
+    xlabel('X');ylabel('Y')
+    title('Difference of U1 and U2','fontsize',18)
     
     %Plot evals over windows
-    % tt = linspace(1,N);
-    % figure;
-    % scatter(tt,s1,'ok')
-    % 
-    % figure;
-    % plot(s2)
-    % 
-    % figure;
-    % plot(s3)
-    % 
-    % figure;
-    % plot(s4)
-end
-
-%Plot dominant eval
-if false
-    figure
-    plot(abs(domEval))
-    title('Modulus of dominant eval','fontsize',18)
+    tt = linspace(1,N);
+    figure;
+    scatter(tt,s1,'ok')
     
-    figure
-    plot(abs(angle(domEval)))
-    title('Angle of dominant eval','fontsize',18)
+    figure;
+    plot(s2)
+    
+    figure;
+    plot(s3)
+    
+    figure;
+    plot(s4)
 end
-
-
 
 
 %Plot top evals
-
-linS = {'-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--'};
-tim = linspace(1,no_Windows,no_Windows);
-
-
-figure;
-absTopEval = abs(topEval);
-
-for modeNumber = 1:top
+if plot_eval
     
-    plot( tim, absTopEval(modeNumber,:),'linestyle', linS(modeNumber) )
-    hold on;
-
-end
-axis([1 400 .95 1.1])
-hold off;
-title('Modulus of top evals','fontsize',18)
-legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20')
-
-
-
-
-
-
-figure;
-angleTopEval = abs(angle(topEval));
-
-for modeNumber = 1:top
+%     linS = {'-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--','-','--'};
+    linS = {'-','--','-','--','-','--'};
+    tim = linspace(1,no_Windows,no_Windows);
     
-    plot( tim, angleTopEval(modeNumber,:),'linestyle', linS(modeNumber) )
-    hold on;
+    
+    figure;
+    absTopEval = abs(topEval);
+    
+    for modeNumber = 1:top
+        
+        plot( tim, absTopEval(modeNumber,:),'linestyle', linS(modeNumber) )
+        hold on;
+    
+    end
+    axis([1 no_Windows .95 1.1])
+    hold off;
+    title('Modulus of top evals','fontsize',18)
+%     legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20')
+    legend('1','2','3','4','5','6')
+    
 
+
+    figure;
+    angleTopEval = abs(angle(topEval));
+    
+    for modeNumber = 1:top
+        
+        plot( tim, angleTopEval(modeNumber,:),'linestyle', linS(modeNumber) )
+        hold on;
+    
+    end
+    hold off;
+    title('Angle of top evals','fontsize',18)
+%     legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20')
+    legend('1','2','3','4','5','6')
+    
 end
-hold off;
-title('Angle of top evals','fontsize',18)
-legend('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20')
-
 
 
 
