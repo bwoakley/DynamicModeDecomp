abe9238e51102fea55c2a7d63456a91cc058ed30
Bryan <boakley@wisc.edu>
2023-04-12 10:14:27 -0700

    Getting Four truncation

--- Research/Bryan_Code/untitled.m
+++ Research/Bryan_Code/untitled.m
@@ -1,72 +1,104 @@
 clear all;
 close all;
 clc;
-format long;
+%format long;
 
 
-Fs = 1000;            % Sampling frequency                    
-T = 1/Fs;             % Sampling period       
-L = 1500;             % Length of signal
-t = (0:L-1)*T;        % Time vector
+L = 50;               % Length of signal
+T = 1.5;              % Sample Period
 
-S = 0.7*cos(2*pi*50*t) + sin(2*pi*20*t);
+t = (0:L-1)*T/L;        % Time vector
+
+
+% figure;
+% plot(t)
+
+S = 0.7*cos(2*pi*t) + sin(2*pi*2*t);
 
 figure;
 plot(S)
 
 Y = fft(S);
-rescaledY = Y/L;
-
-realCoeff = real(rescaledY);
-imagCoeff = imag(rescaledY);
+% abs(Y)
 
 
+if false
+    rescaledY = Y/L;
+    
+    realCoeff = real(rescaledY);
+    imagCoeff = imag(rescaledY);
+    
+    figure;
+    plot(realCoeff)
+    hold on;
+    plot(imagCoeff)
+    hold off;
+    legend('real','imag')
+    title('raw freq')
+end
+
+kk = 1;     %Use top kk frequencies (2*kk+1 total modes) to approx Eval1
+if 2*kk+1 > L
+    disp('kk too large')
+end
+shiftY = fftshift(Y);
+% abs(shiftY)
+
+if mod(L,2) == 0
+    middle = ceil(L/2) + 1;
+else 
+    middle = ceil(L/2);
+end
 
 
 
-k = (0:L-1)/1.5;
-% k = (0:L/2)/1.5;
+range = middle - kk: middle + kk ;
 
-figure;
-plot(k, realCoeff)
-hold on;
-plot(k,imagCoeff)
-hold off;
-legend('real','imag')
+truncY = shiftY( range );
+
+shiftTruncY = ifftshift(truncY);
+
+ifft(shiftTruncY)
 
 
 
+
+
+
+% k = (0:L-1)/1.5;
+% 
+% figure;
+% plot(k, realCoeff)
+% hold on;
+% plot(k,imagCoeff)
+% hold off;
+% legend('real','imag')
+% title('rescaled freq')
 
 % rescaledY
 
 
+if false
+    kk = 1;     %Use kk modes to approx Eval1
+    approxEval1 = 0;
+    for i = 1:kk
+        approxEval1 = approxEval1 + 0;
+    end
+    
+    % approxEval1 = ifft(Y, 1500);
+    approxEval1 = ifft(Y, 100,'symmetric');
+    
+    figure;
+    plot( real(approxEval1) )
+    hold on;
+    plot( imag(approxEval1) )
+    legend('real','imag')
+    
+    
+    % figure;
+    % plot( abs(S - approxEval1) )
+    % title('error')
 
 
-
-
-
-
-
-kk = 1;     %Use kk modes to approx Eval1
-approxEval1 = 0;
-for i = 1:kk
-    approxEval1 = approxEval1 + 0;
 end
-
-% approxEval1 = ifft(Y, 1500);
-approxEval1 = ifft(Y, 100,'symmetric');
-
-figure;
-plot( real(approxEval1) )
-hold on;
-plot( imag(approxEval1) )
-legend('real','imag')
-
-
-% figure;
-% plot( abs(S - approxEval1) )
-% title('error')
-
-
-
 
